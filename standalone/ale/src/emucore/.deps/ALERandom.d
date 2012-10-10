02db995abbbf764140d29870a9ad69e127577e5f
chrisdonahue <christopherdonahue@gmail.com>
2012-10-10 18:25:11 -0500

    removed old standalone directory

--- standalone/ale/src/emucore/.deps/ALERandom.d
+++
@@ -1,6 +0,0 @@
-src/emucore/ALERandom.o: src/emucore/ALERandom.cxx \
- src/emucore/ALERandom.hxx src/emucore/m6502/src/bspf/src/bspf.hxx
-
-src/emucore/ALERandom.hxx:
-
-src/emucore/m6502/src/bspf/src/bspf.hxx:
