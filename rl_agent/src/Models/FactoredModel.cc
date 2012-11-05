5a1077bda5ceb759d2957745da1e5a79e6545396
chrisdonahue <christopherdonahue@gmail.com>
2012-11-05 12:19:46 -0600

    Minor additional change

--- rl_agent/src/Models/FactoredModel.cc
+++ rl_agent/src/Models/FactoredModel.cc
@@ -328,7 +328,8 @@ bool FactoredModel::updateWithExperiences(std::vector<experience> &instances)
 				// split the outcome and rewards up
 				// into each vector
 				cp.out = e.next[j];
-				stateData[j][nonTermIndex] = cp;
+                if (e.validForModels[j])
+				    stateData[j][nonTermIndex] = cp;
 
 				// for dep trees, add this models target to next model's input
 				if (dep)
